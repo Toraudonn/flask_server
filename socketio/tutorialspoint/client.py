6826838a2280f67533f8a5bacb654689e47022af
Toraudonn <toraudonn@gmail.com>
2018-10-04 00:33:40 +0900

    tcp socket demo

---
+++ socketio/tutorialspoint/client.py
@@ -0,0 +1,31 @@
+import socket
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name
+host = socket.gethostname()                           
+
+port = 9999
+
+# connection to hostname on the port.
+s.connect((host, port))                               
+
+# Receive no more than 1024 bytes
+# msg = s.recv(1024)    
+def recvall(sock):
+    '''for large data, send incrementally'''
+    BUFF_SIZE = 4 # 4 KiB
+    data = b''
+    while True:
+        part = sock.recv(BUFF_SIZE)
+        data += part
+        if len(part) < BUFF_SIZE:
+            # either 0 or end of data
+            break
+    return data
+
+msg = recvall(s)
+
+s.close()
+print(msg.decode('ascii'))
